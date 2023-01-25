23f8c67296d84fcecd9fe887c80e84e15472f563
Brandon Wie <brandonwie.cs@gmail.com>
2023-01-25 22:24:54 +0900

    Add p tag

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
+        <p>Hello there</p>
       </header>
     </div>
   );
